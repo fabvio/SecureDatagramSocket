3ffb72e319abef795fb89f5c9f61146cfb0600ad
Fabio Pizzati <pizzati.fabio@gmail.com>
2016-08-17 00:06:06 +0200

    fixed received packet size

--- src/SecureDatagramSocket.java
+++ src/SecureDatagramSocket.java
@@ -1,15 +1,11 @@
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-import java.security.SignatureException;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.InvalidParameterSpecException;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
@@ -33,6 +29,11 @@ public class SecureDatagramSocket extends DatagramSocket {
 		this.password = _password;
 	}
 	
+	public SecureDatagramSocket(String _password, int PORT) throws SocketException{
+		super(PORT);
+		this.password = _password;
+	}
+	
 	@Override
 	public void send(DatagramPacket p) throws IOException  {
 		byte[] encryptedMessage = null;
@@ -43,28 +44,31 @@ public class SecureDatagramSocket extends DatagramSocket {
 			e.printStackTrace();
 		}
 		
-		byte[] mac = null;
-		try {
-			mac = HMAC.calculate(p.getData(), this.password);
-		} catch (InvalidKeyException | SignatureException | NoSuchAlgorithmException e) {
-			e.printStackTrace();
-		}
-
 		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 		outputStream.write(encryptedMessage);
-		outputStream.write(mac);
 		
 		p.setData(outputStream.toByteArray());
-		
+			
 		super.send(p);
+		
 	}
 	
 	@Override
 	public void receive(DatagramPacket p) throws IOException {
 		super.receive(p);
 		
+		// The size changes if the message is decrypted
+		int size = p.getLength();
 		
-		
+		byte[] decryptedMessage = null;
+		try {
+			decryptedMessage = AES.decrypt(p.getData(), this.password);
+		} catch (InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException
+				| InvalidAlgorithmParameterException | IllegalBlockSizeException | BadPaddingException e) {
+			e.printStackTrace();
+		}
+		p.setData(decryptedMessage);
+		p.setLength(size);
 	}
 	
 	

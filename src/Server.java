b9a4f6a9700ee676e61178322860f54813871b97
Fabio Pizzati <pizzati.fabio@gmail.com>
2016-08-17 00:03:59 +0200

    Fixed byte buffer

--- src/Server.java
+++ src/Server.java
@@ -1,19 +1,22 @@
 import java.io.IOException;
 import java.net.DatagramPacket;
+import java.util.Arrays;
 
 public class Server {
-
-	public Server() throws IOException{
-		super();
-		byte[] buf = new byte[1000];
-	    DatagramPacket p = new DatagramPacket(buf, buf.length);
-	    SecureDatagramSocket socket = new SecureDatagramSocket("CULO", 4567);
+	
+	public static void main(String[] args) throws IOException{
+		byte[] buf = new byte[256];
+	    DatagramPacket p = new DatagramPacket(buf, 256);
+	    SecureDatagramSocket socket = new SecureDatagramSocket("CULO", 12398);
 	    System.out.println("Server started");
 	    while(true){
 	    	socket.receive(p);
-	    	if(new String(p.getData()).equals("byte")){
+	    	
+	    	if(new String(p.getData()).equals("bye")){
 	    		break;
 	    	}
+	    	p.setData(Arrays.copyOfRange(p.getData(), 0, p.getLength()));
+	    	System.out.println("Received:" + new String(p.getData(), "UTF8"));
 	    }
 	    
 	    socket.close();
